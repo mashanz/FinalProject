c3c9c3747eb95237c58c82635b3da53fa532c219
Hanjara Cahya Adhyatma <mangatkk@gmail.com>
2017-05-24 10:28:12 +0700

    Add files via upload

---
+++ Code/cmp.py
@@ -0,0 +1,37 @@
+#######################################################################
+#                          Import Sistem
+#######################################################################
+import os
+
+#######################################################################
+#                          MODULE UTAMA
+#######################################################################
+top  	= "top" # Ganti sesuai nama module yang akan di kompile
+
+#######################################################################
+#                    	MAIN PROSES (JANGAN GANTI)
+#######################################################################
+dir  	= "module/"
+file 	= "cmp.ys"
+liberty = "lib/cmos_cells.lib"
+compiled= "cmp.v"
+def read():
+	x=0
+	out  = open(file,"w")
+	rtl_files = os.listdir(dir)
+	while(x<len(rtl_files)):
+		print(rtl_files[x])
+		out.write("read_verilog " + dir + rtl_files[x] + "\n")
+		x+=1
+	out.write("hierarchy -check -top "+top+"\n")
+	out.write("proc; opt; memory; fsm; opt\n")
+	out.write("techmap; opt\n")
+	out.write("dfflibmap -liberty "+liberty+"\n")
+	out.write("abc -liberty "+liberty+"\n")
+	out.write("opt\n")
+	out.write("write_verilog -norename -noattr "+compiled+"\n")
+	out.close()
+	os.system("yosys " + file) 
+if __name__ == "__main__":
+	read()
+	
